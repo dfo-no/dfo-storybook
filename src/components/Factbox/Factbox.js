a47c7b9e11cf0067726aad0f95eafab8e3b2eef5
Nils Norman Haukås <mail@nilsnh.no>
2018-11-16 17:14:10 +0100

    fix component name

--- src/components/Factbox/Factbox.js
+++ src/components/Factbox/Factbox.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Factbox.scss";
 
-export default function Checkbox({ children = [], ...rest }) {
+export default function Factbox({ children = [], ...rest }) {
   return children.length > 0 ? (
     <div className="dfo-factbox">{children}</div>
   ) : null;
