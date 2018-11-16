be4baec6fb239589f28c68c8ab708d5c29b93e29
Nils Norman Haukås <mail@nilsnh.no>
2018-11-16 16:38:21 +0100

    update typography example

--- src/pages/TypographyPage.js
+++ src/pages/TypographyPage.js
@@ -4,20 +4,22 @@ import "../components/reset";
 export default function TypographyPage() {
   return (
     <div>
-      <h2>Kontaktinfo</h2>
-      <p>Har du spørsmål om innholdet på denne siden?</p>
+      <h1>Dette er en h1 tittel</h1>
+      <h2>Dette er en h2 tittel</h2>
+      <h3>Dette er en h3 tittel</h3>
+      <br />
       <p>
-        <strong>Kontakt kundesenter lønn</strong>{" "}
+        Regelverk og systemer{" "}
+        <a href="https://dfo.no/">Direktoratet for økonomistyring</a> forvalter
+        økonomiregelverket, utredningsinstruksen og to av statens
+        konsernsystemer. Dette er oppgaver som er delegert til oss fra
+        Finansdepartementet. <em>Vi er opptatt av å forbedre</em> regelverkene
+        og systemene slik at det i størst mulig grad bidrar til{" "}
+        <strong>effektiv ressursbruk i staten.</strong> Våre forslag til
+        forbedringer skal være tuftet både på faglige utviklingstrekk nasjonalt
+        og internasjonalt, og på våre erfaringer med bruken av regelverkene og
+        konsernsystemene.
       </p>
-      <ul>
-        <li>
-          e-post <a href="/#">lonn@dfo.no</a>
-        </li>
-        <li>
-          <a href="/#">Kundesenter på nett</a>
-        </li>
-        <li>telefon 123 45 678</li>
-      </ul>
       <p>
         <strong>Kontakt kundesenter regnskap</strong>
       </p>
