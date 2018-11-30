940794e1bd6df31ca06fac4ffa6cc2918916cda9
Nils Norman Haukås <mail@nilsnh.no>
2018-11-30 16:09:10 +0100

    fix .use is undefined error in webpack.config when running npm run build

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,22 +1,24 @@
 // load the default config generator.
-var join = require('path').join
-var root = join(__dirname, '..');
+var join = require("path").join;
+var root = join(__dirname, "..");
 
 module.exports = (baseConfig, env, defaultConfig) => {
-  const styleRuleIndex = defaultConfig.module.rules.findIndex(
-    rule => 'apekatt.scss'.match(rule.test)
+  const sassRule = defaultConfig.module.rules.find(rule =>
+    "apekatt.scss".match(rule.test)
   );
 
   // Add sass-resource-loader to style rule
-  defaultConfig.module.rules[styleRuleIndex].use.push({
-    loader: 'sass-resources-loader',
-    options: {
-      resources: [
-        join(root, './sass/reset.scss'),
-        join(root, './sass/fonts.scss'),
-      ],
-    },
-  })
+  if (sassRule && sassRule.use) {
+    sassRule.use.push({
+      loader: "sass-resources-loader",
+      options: {
+        resources: [
+          join(root, "./sass/reset.scss"),
+          join(root, "./sass/fonts.scss")
+        ]
+      }
+    });
+  }
 
   return defaultConfig;
-}
+};
