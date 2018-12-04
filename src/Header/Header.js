1bf27034dd9886c947ba71304f39e053fb57a96e
Jenny Bonsak <jenny.bonsak@gmail.com>
2018-12-04 12:15:41 +0100

    Make menu responsive, initiate burger

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import * as classNames from "classnames";
 import "./Header.scss";
 
@@ -8,24 +9,22 @@ import logo from "./dfo-logo.svg";
 export default function Header({
   links = []
   }) {
-    const classes = classNames("dfo-header");
-
-  return (
-    <header className={classes}>
-      <div className="container container--flex">
-        <div className="dfo-header__wrapper">
-          <div className="dfo-header__wrapper__inner">
-            <img src={logo} alt="Direktoratet for Økonomistyrings logo" />
-          </div>
-            <div className="dfo-header__menu">
-              <div className="dfo-menu">
-                {links.map((item, i) => (
-                <a className="dfo-menu__item" key={i} href={item.link}>{item.title}</a>
-                ))}
+    return (
+      <header className="dfo-header dfo-header--menu-open">
+        <div className="container container--flex">
+          <div className="dfo-header__wrapper">
+            <div className="dfo-header__wrapper__inner">
+              <img src={logo} alt="Direktoratet for Økonomistyrings logo" />
+            </div>
+              <div className="dfo-header__menu">
+                <div className="dfo-menu">
+                  {links.map((item, i) => (
+                  <a className="dfo-menu__item" key={i} href={item.link}>{item.title}</a>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-    </header>
-  );
+      </header>
+    );
 }
--- src/Header/Header.stories.js
+++ src/Header/Header.stories.js
@@ -13,9 +13,8 @@ storiesOf("Header", module)
     "Header",
     () => (
       <Header
-
         links={object("Links", [
-          { link: "url", title: "Link title"},
+          { link: "url", title: "Link title" },
           { link: "url", title: "Link title" },
           { link: "url", title: "Link title" }
         ])}
@@ -25,4 +24,3 @@ storiesOf("Header", module)
       css: require("!to-string-loader!css-loader!sass-loader!./Header.scss")
     }
   );
-
