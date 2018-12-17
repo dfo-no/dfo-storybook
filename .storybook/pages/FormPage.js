8ef8dac3f146b49e2782da34eca4844f67611eeb
Nils Norman Haukås <mail@nilsnh.no>
2018-12-17 16:10:15 +0100

    start reworking form

--- .storybook/pages/FormPage.js
+++ .storybook/pages/FormPage.js
@@ -1,21 +1,14 @@
 import React from "react";
-import { Form, Input, Checkbox, Button } from "../../src/index";
+import { Form, Input, Checkbox, Button, RadioButtons } from "../../src/index";
 
 export default function FormPage() {
   return (
     <Form method="post">
-      <h2>Meld deg på vårt nyhetsbrev</h2>
-      <Input
-        label="Din e-postadresse. (Av sikkerhetsgrunner kan du ikke endre den her.)"
-        type="email"
-        name="email"
-        defaultValue="kari@dfo.no"
-        disabled
-      />
-      <Input label="Fornavn:" type="text" name="fields[firstName]" />
-      <Input label="Etternavn:" type="text" name="fields[lastName]" />
-      <Input label="Virksomhet:" type="text" name="fields[organisation]" />
-      <strong>Abonner på :</strong>
+      <Form.Title>DFØs Styringskonferanse 2019</Form.Title>
+      <Form.SectionTitle>Kontaktinformation:</Form.SectionTitle>
+      <Input label="Fornavn" type="text" name="fields[firstName]" value="" />
+      <Input label="Etternavn" type="text" name="fields[lastName]" value="" />
+      <strong>Abonner på:</strong>
       <Checkbox
         label="Nyhetsbrev om virksomhets- og økonomistyring"
         name="fields[customerNoteInterests][]"
--- src/Form/Form.js
+++ src/Form/Form.js
@@ -5,3 +5,17 @@ export default function Form(props) {
   const { children = [] } = props;
   return <form className="dfo-form">{children}</form>;
 }
+
+Form.Title = function FormTitle({ children = [] }) {
+  if (children.length === 0) {
+    return null;
+  }
+  return <h2 className="dfo-form-title">{children}</h2>;
+};
+
+Form.SectionTitle = function FormSectionTitle({ children = [] }) {
+  if (children.length === 0) {
+    return null;
+  }
+  return <p className="dfo-form-section-title">{children}</p>;
+};
