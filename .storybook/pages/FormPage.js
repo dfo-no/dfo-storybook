0281b4b0dcd7cdb3815a791b8f130bdd00133193
Tatsumi Suzuki <tatsumi.suzuki@netlife.com>
2018-12-20 12:33:00 +0100

    Refactor form and reduce markup and css footprint.

--- .storybook/pages/FormPage.js
+++ .storybook/pages/FormPage.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Form, Input, Checkbox, Button, RadioButtons } from "../../src/index";
+import { Form, Input, Button, RadioButtons } from "../../src/index";
 
 export default function FormPage() {
   return (
     <Form method="post">
-      <h2 className="dfo-form-title">DFØs Styringskonferanse 2019</h2>
-      <fieldset className="dfo-form-fieldset">
-        <legend className="dfo-form-legend">Kontaktinformation:</legend>
+      <h2>DFØs Styringskonferanse 2019</h2>
+      <fieldset className="dfo-form__fieldset">
+        <legend>Kontaktinformation:</legend>
         <Input label="Fornavn" type="text" name="fields[firstName]" value="" />
         <Input
           label="Etternavn"
@@ -16,19 +16,20 @@ export default function FormPage() {
           value=""
         />
       </fieldset>
-      <fieldset className="dfo-form-fieldset">
-        <legend className="dfo-form-legend">Er du i en lederstilling?</legend>
+      <fieldset className="dfo-form__fieldset">
         <RadioButtons
           name="isLeader"
+          legend="Er du i en lederstilling?"
           options={[
             { label: "Ja", value: "yes", defaultChecked: true },
             { label: "Nei", value: "no" }
           ]}
         />
+        <Input label="Fornavn" type="text" name="fields[firstName]" value="" />
       </fieldset>
-      <div className="dfo-form-submit-wrapper">
+      <fieldset className="dfo-form__fieldset dfo-form__fieldset--reset-background">
         <Button type="submit">Meld på</Button>
-      </div>
+      </fieldset>
     </Form>
   );
 }
--- src/RadioButtons/RadioButtons.js
+++ src/RadioButtons/RadioButtons.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./RadioButtons.scss";
 
-export default function RadioButtons({ name, options = [] }) {
+export default function RadioButtons({ name, legend, options = [] }) {
   return (
     <div
-      className="dfo-radio-buttons inner-wrapper"
+      className="dfo-radio-buttons"
       role="radiogroup"
       aria-label={name}
     >
+      {
+        legend && (
+          <legend>{legend}</legend>
+        )
+      }
       {options.map(({ label, value, ...rest }) => {
         const key = `${name}-${value}`;
         return (
