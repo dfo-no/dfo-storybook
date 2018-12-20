c821673afcc12cb4747022a5dc4565e94ccf21cf
Kristoffer Brabrand <kristoffer@brabrand.no>
2018-12-20 15:36:16 +0100

    Add the form page

---
+++ src/Form/FormPage.stories.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import {
+  Form,
+  Input,
+  Button,
+  RadioButtons,
+} from '../index';
+
+storiesOf('Forms', module)
+  .add('Complete form', () => (
+    <Form method="post">
+      <h2>DFØs Styringskonferanse 2019</h2>
+      <fieldset className="dfo-form__fieldset">
+        <legend>Kontaktinformation:</legend>
+        <Input label="Fornavn" type="text" name="fields[firstName]" value="" />
+        <Input
+          label="Etternavn"
+          type="text"
+          error="Vennligst fyll ut etternavn"
+          name="fields[lastName]"
+          value=""
+        />
+      </fieldset>
+      <fieldset className="dfo-form__fieldset">
+        <RadioButtons
+          name="isLeader"
+          legend="Er du i en lederstilling?"
+          options={[
+            { label: 'Ja', value: 'yes', defaultChecked: true },
+            { label: 'Nei', value: 'no' },
+          ]}
+        />
+        <Input label="Fornavn" type="text" name="fields[firstName]" value="" />
+      </fieldset>
+      <fieldset className="dfo-form__fieldset dfo-form__fieldset--reset-background">
+        <Button type="submit">Meld på</Button>
+      </fieldset>
+    </Form>
+  ));
