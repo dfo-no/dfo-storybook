7f845d5dea8100e190509d0e7c8e4e938cf7a984
Kristoffer Brabrand <kristoffer@brabrand.no>
2018-12-21 16:17:18 +0100

    Comment out the section linking to the design guide for now. It's not yet public

--- .storybook/pages/WelcomePage.js
+++ .storybook/pages/WelcomePage.js
@@ -6,8 +6,10 @@ export default function WelcomePage() {
       <h1><code>@dfo/components</code> 🚀</h1>
       <p>Documentation of components built for Direktoratet for økonomistyring (DFØ).</p>
 
+      {/*
       <h2>Looking for a design guide?</h2>
       <p>While this documentation is specifically for the digital surfaces of DFØ, the DFØ website offers a more <a href="https://dfo.no/om-dfo/designmanualen">general design guide</a> for the DFØ brand exists on.</p>
+      */}
 
       <h2>Using the components</h2>
       <p>dfo.no and the DFØ intranet is built using the React components distributed through <a href="https://www.npmjs.com/package/@dfo/components">npm</a> – see link to github below for instructions on how to do that, if you're building a react app. If you're building something that's not a React application you can still get styles and markup for the different components.</p>
