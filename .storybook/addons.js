e9cf806ad0aa308b31fef894f8a2eb22fda6e825
Kristoffer Brabrand <kristoffer@brabrand.no>
2018-12-21 16:40:14 +0100

    Change link in top left corner

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -4,5 +4,6 @@ import "@storybook/addon-links/register";
 import "@storybook/addon-viewport/register";
 import "@storybook/addon-knobs/register";
 import "@storybook/addon-a11y/register";
+import '@storybook/addon-options/register';
 import "react-storybook-addon-static-markup/register";
 import "./static-css-addon/register";
--- .storybook/stories.index.js
+++ .storybook/stories.index.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { storiesOf, addDecorator } from "@storybook/react";
+import { withOptions } from '@storybook/addon-options';
+
 
 import { Padding, ShowStaticMarkup } from "./stories.decorators";
 import { WelcomePage, TypographyPage, ColorsPage } from "./pages";
 
+addDecorator(
+  withOptions({
+    name: 'github repo',
+    url: 'https://github.com/netliferesearch/dfo-storybook'
+  })
+);
 addDecorator(Padding);
 addDecorator(ShowStaticMarkup);
 
