38de7d5a10c8830e38f4b7b04ca87a91beefe4c0
Nils Anders Hvinden <nils.anders.hvinden@cloudworks.no>
2019-04-11 14:53:42 +0200

    Added placeholder prop for Select component.

--- src/Select/Select.js
+++ src/Select/Select.js
@@ -4,18 +4,22 @@ import PropTypes from 'prop-types';
 import './Select.scss';
 
 export default function Select({
-  name, label, options, children, initiallyEmpty, ...rest
+  name, label, options, children, placeholder, value, ...rest
 }) {
   if (children) {
     return <div className="dfo-select">{children}</div>;
   }
+
+
+  const className = (value !== '') ? undefined : 'displayPlaceholder';
+
   return (
     <div className="dfo-select">
       {label && <label htmlFor={`select-${name}`}>{label}</label>}
-      <select id={`select-${name}`} name={name} {...rest}>
-        { initiallyEmpty && <option key="" value="" disabled></option> }
+      <select id={`select-${name}`} name={name} value={value} className={className} {...rest}>
+        { (placeholder || placeholder === '') && <option key={placeholder} value='' disabled>{placeholder}</option> }
         { options.map(option => {
-            return (typeof option === "object") ? <option key={option.value} value={option.value}>{option.text}</option> : <option key={option}>{option}</option>;
+            return (typeof option === 'object') ? <option key={option.value} value={option.value}>{option.text}</option> : <option key={option}>{option}</option>;
         })}
       </select>
     </div>
@@ -27,6 +31,7 @@ Select.propTypes = {
   label: PropTypes.string,
   options: PropTypes.array,
   children: PropTypes.any,
+  placeholder: PropTypes.string,
 };
 
 Select.defaultProps = {
