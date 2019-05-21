161bd1053d2782c362c8a7bac64b034e0306c38d
Nils Anders Hvinden <nils.anders.hvinden@cloudworks.no>
2019-05-21 18:55:30 +0200

    Added error message support for Select

--- src/Select/Select.js
+++ src/Select/Select.js
@@ -1,28 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 import './Select.scss';
 
 export default function Select({
-  name, label, valueAttr = 'value', textAttr = 'text', options, children, placeholder, value, ...rest
+  name, valueAttr = 'value', textAttr = 'text', label, options, children, placeholder, value, error, ...rest
 }) {
   if (children) {
     return <div className="dfo-select">{children}</div>;
   }
 
-
   const className = (value !== '') ? undefined : 'displayPlaceholder';
 
   return (
-    <div className="dfo-select">
-      {label && <label htmlFor={`select-${name}`}>{label}</label>}
-      <select id={`select-${name}`} name={name} value={value} className={className} {...rest}>
+    <label
+      htmlFor={`select-${name}`}
+      className={classnames('dfo-input', { 'dfo-input--is-error': error })}
+    >
+      <div
+        className="dfo-select dfo-error-wrapper"
+        aria-live="assertive"
+        aria-relevant="additions removals"
+      >
+      {label}
+      <select aria-invalid={!!error} id={`select-${name}`} name={name} value={value} className={className} {...rest}>
         { (placeholder || placeholder === '') && <option key={placeholder} value='' disabled>{placeholder}</option> }
         { options.map(option => {
             return (typeof option === 'object') ? <option key={option[valueAttr]} value={option[valueAttr]}>{option[textAttr]}</option> : <option key={option}>{option}</option>;
         })}
       </select>
+      {error && <span className="dfo-error-wrapper__error">{error}</span>}
     </div>
+    </label>
   );
 }
 
