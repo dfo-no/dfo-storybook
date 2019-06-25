389b4db6ebb398d2a217d4fae7674634b459de17
Nils Norman Haukås <mail@nilsnh.no>
2019-06-25 13:03:40 +0200

    Adjust alertbox content line height. Resolves #1.

--- src/Alertbox/Alertbox.stories.js
+++ src/Alertbox/Alertbox.stories.js
@@ -16,6 +16,12 @@ storiesOf('Alertbox', module)
       <Alertbox>
         <p>This is an important announcement.</p>
         <p>It is perfectly fine to have more than one paragraph.</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Nam urna dui, accumsan a egestas vel, facilisis fermentum enim.
+          Nam congue lobortis risus. Praesent vitae dignissim eros. Aenean
+          sapien urna, egestas sagittis quam vitae, tempor volutpat eros.
+        </p>
       </Alertbox>
     ),
     {
