170fc643ee38a50f1f8150156f9bd36eec529e31
Nils Norman Haukås <mail@nilsnh.no>
2019-06-26 08:56:03 +0200

    extract Select options parse logic into separate function

--- src/Select/Select.js
+++ src/Select/Select.js
@@ -4,41 +4,50 @@ import classnames from 'classnames';
 
 import './Select.scss';
 
+// Options can be expressed as either array of strings or an object with keys.
+function parseSelectOptions({ options, displayAttr }) {
+  if (Array.isArray(options)) {
+    return options.map(option => (
+      <option key={option}>{option}</option>
+    ));
+  }
+  if (typeof options === 'object') {
+    // Options as normalized object
+    return Object.entries(options).map(([key, value]) => (
+      <option key={key} value={key}>
+        {value[displayAttr]}
+      </option>
+    ));
+  }
+  return [];
+}
+
 export default function Select({
-  name, displayAttr, label, options, children, placeholder, value, error, ...rest
+  name,
+  displayAttr,
+  label,
+  options,
+  children,
+  placeholder,
+  value,
+  error,
+  ...rest
 }) {
   if (children) {
     return <div className="dfo-select">{children}</div>;
   }
 
-  const PLACEHOLDER_KEY = 'placeholder'  
-  var placeholderOptionSelected = false
-  var placeholderVisibleStyle = undefined;
-  
+  const PLACEHOLDER_KEY = 'placeholder';
+  let placeholderOptionSelected = false;
+  let placeholderVisibleStyle;
+
   // Placeholder is specified and no value selected => display the placeholder
   if (placeholder && !value) {
-    placeholderOptionSelected = PLACEHOLDER_KEY
-    placeholderVisibleStyle = 'dfo-select__placeholder'
+    placeholderOptionSelected = PLACEHOLDER_KEY;
+    placeholderVisibleStyle = 'dfo-select__placeholder';
   }
 
-  
-  var selectOptions = null
-  
-  // Options as array of plain strings
-  if (Array.isArray(options)) {
-    
-    selectOptions = options.map(option => (
-      <option key={option}>{option}</option>
-    ))
-
-  // Options as normalized object
-  } else if (typeof options === 'object') {
-    
-    selectOptions = Object.entries(options).map(([key, value]) => (
-      <option key={key} value={key}>{value[displayAttr]}</option>
-    ))
-  } 
-
+  const selectOptions = parseSelectOptions({ options, displayAttr });
 
   return (
     <label
@@ -50,19 +59,26 @@ export default function Select({
         aria-live="assertive"
         aria-relevant="additions removals"
       >
-
         {label}
 
-        <select aria-invalid={!!error} id={`select-${name}`} name={name} value={value || placeholderOptionSelected || undefined} className={placeholderVisibleStyle} {...rest}>
-          
-          { (placeholder || placeholder === '') && <option key={placeholder} value={PLACEHOLDER_KEY} disabled>{placeholder}</option> }
-          
-          { selectOptions }
+        <select
+          aria-invalid={!!error}
+          id={`select-${name}`}
+          name={name}
+          value={value || placeholderOptionSelected || undefined}
+          className={placeholderVisibleStyle}
+          {...rest}
+        >
+          {(placeholder || placeholder === '') && (
+            <option key={placeholder} value={placeholder} disabled>
+              {placeholder}
+            </option>
+          )}
 
+          {selectOptions}
         </select>
 
         {error && <span className="dfo-error-wrapper__error">{error}</span>}
-
       </div>
     </label>
   );
@@ -73,10 +89,11 @@ Select.propTypes = {
   label: PropTypes.string,
   options: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.string),
-    PropTypes.object
+    PropTypes.object,
   ]),
   children: PropTypes.any,
   placeholder: PropTypes.string,
+  displayAttr: PropTypes.string,
 };
 
 Select.defaultProps = {
@@ -84,5 +101,5 @@ Select.defaultProps = {
   label: null,
   options: [],
   displayAttr: 'text',
-  placeholder: null
+  placeholder: null,
 };
