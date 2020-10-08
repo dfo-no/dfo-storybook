f9c4b8662bd3afba561c490ecdb593b8a6154b82
Haakon Borch <haakon.borch@netlife.com>
2020-10-08 09:29:53 +0200

    :fire: remove unused css and stories

--- src/FloatingActionButton/FloatingActionButton.stories.js
+++ src/FloatingActionButton/FloatingActionButton.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { text, boolean } from '@storybook/addon-knobs';
+import { text } from '@storybook/addon-knobs';
 import PropTypes from 'prop-types';
 
 import { FloatingActionButton } from '.';
@@ -23,62 +23,14 @@ const options = {
   /* eslint-enable import/no-webpack-loader-syntax */
 };
 
-storiesOf('FloatingActionButton', module)
-  .add(
-    'Basic',
-    () => (
-      <div style={{ position: 'absolute bottom 0' }}>
-        <FloatingActionButton onClick={action('clicked')} SubComponent={Chat}>
-          {text('Text', 'Sign up for our newsletter')}
-        </FloatingActionButton>
-      </div>
-    ),
-    options,
-  )
-  .add(
-    'Call to action',
-    () => (
-      <FloatingActionButton cta={boolean('cta', true)} onClick={action('clicked')}>
+storiesOf('FloatingActionButton', module).add(
+  'Basic',
+  () => (
+    <div style={{ position: 'absolute bottom 0' }}>
+      <FloatingActionButton onClick={action('clicked')} SubComponent={Chat}>
         {text('Text', 'Sign up for our newsletter')}
       </FloatingActionButton>
-    ),
-    options,
-  )
-  .add(
-    'Inverted light version',
-    () => (
-      <FloatingActionButton invert={boolean('Invert', true)} onClick={action('clicked')}>
-        {text('Text', 'Sign up for our newsletter')}
-      </FloatingActionButton>
-    ),
-    options,
-  )
-  .add(
-    'Danger button',
-    () => (
-      <FloatingActionButton danger={boolean('danger', true)} onClick={action('clicked')}>
-        {text('Text', 'Delete all dem datas 😰')}
-      </FloatingActionButton>
-    ),
-    options,
-  )
-  .add(
-    'CTA link disguised as button',
-    () => (
-      <FloatingActionButton plainLink={boolean('Link', true)} cta={boolean('cta', true)} href="/somewhere">
-        {text('Text', 'Sign up for our newsletter')}
-      </FloatingActionButton>
-    ),
-    options,
-  )
-  .add(
-    'Disabled',
-    () => (
-      <div>
-        <FloatingActionButton onClick={action('clicked')} disabled>
-          {text('Text', 'Sign up for our newsletter')}
-        </FloatingActionButton>
-      </div>
-    ),
-    options,
-  );
+    </div>
+  ),
+  options,
+);
