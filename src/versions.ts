52fd82b82df97951e950d09ef174aa5b5b60a973
Brede Fladen DFØ <brede.fladen@dfo.no>
2026-06-19 10:24:46 +0200

    build(deps-dev): Storybook upgraded to v10.4.6 by 'npx storybook@latest upgrade'.

--- src/versions.ts
+++ src/versions.ts
@@ -1,3 +1,3 @@
-export const dfo_version = '10.0.3';
+export const dfo_version = '10.4.0';
 export const react_version = '19.2.6';
-export const storybook_version = '10.3.6';
+export const storybook_version = '10.4.6';
